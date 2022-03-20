896b9189fdf7fd87b873433b319956a693c33d50
Hunter Lee Brown <hunterleebrown@gmail.com>
2022-03-20 15:31:47 -0700

    private

--- Sources/iaAPI/Service/IAService.swift
+++ Sources/iaAPI/Service/IAService.swift
@@ -158,7 +158,7 @@ public class IAService {
         return parameters
     }
 
-    fileprivate func mockSearch(completion:@escaping SearchResponse) {
+    private func mockSearch(completion:@escaping SearchResponse) {
         if let data = IAStatic.searchResult.data(using: .utf8) {
             let decoder = JSONDecoder()
             let jsonData = try! decoder.decode(IASearchResults.self, from: data)
@@ -167,7 +167,7 @@ public class IAService {
         }
     }
     
-    fileprivate func mockArchiveDoc(completion:@escaping ArchiveDocResponse) {
+    private func mockArchiveDoc(completion:@escaping ArchiveDocResponse) {
         if let data = IAStatic.archiveDoc.data(using: .utf8) {
             let decoder = JSONDecoder()
             let jsonData = try! decoder.decode(IAArchiveDoc.self, from: data)
